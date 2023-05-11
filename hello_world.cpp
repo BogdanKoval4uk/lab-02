6fe58fe6f3c9e04b04ef4c9a557708355e2d32c5
bogdan <bboggammerr@gmail.com>
2023-05-11 13:17:00 +0300

    sfsfs

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
-
-int main(){
-int a;
-std::cout << "What's your name?";                     //rjvvtyn
-std::cin >> a;
-std::cout << "Hello world from" << a;                 //fdbvdbdg
- return 0;
+int
+main()
+{
+  int a;
+  std::cout << "What's your name?"; // rjvvtyn
+  std::cin >> a;
+  std::cout << "Hello world from" << a; // fdbvdbdg
+  return 0;
 }
